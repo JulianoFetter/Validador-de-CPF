8209bf4be2eb07c91a9a843c875d36b2dd9e9ce2
Juliano Fetter <julianofetter@yahoo.com.br>
2023-06-28 20:18:27 -0300

    Gerador de CNPJ funcional

--- JS/geradorCnpj.js
+++ JS/geradorCnpj.js
@@ -1,38 +1,36 @@
 function gerardorCnpj() {
-    var cnpj = '';
+  var cnpj = '';
     
-    // Gera os 12 primeiros dígitos de forma aleatória
-    for (var i = 0; i < 12; i++) {
-      cnpj += Math.floor(Math.random() * 10);
-    }
-    
-    // Calcula o primeiro dígito verificador
-    var soma = 0;
-    var peso = 2;
-    
-    for (var i = 11; i >= 0; i--) {
-      soma += parseInt(cnpj.charAt(i)) * peso;
-      peso = (peso === 9) ? 2 : peso + 1;
-    }
-    
-    var digitoVerificador1 = 11 - (soma % 11);
-    cnpj += (digitoVerificador1 < 10) ? digitoVerificador1 : 0;
-    
-    // Calcula o segundo dígito verificador
-    soma = 0;
-    peso = 2;
-    
-    for (var i = 12; i >= 0; i--) {
-      soma += parseInt(cnpj.charAt(i)) * peso;
-      peso = (peso === 9) ? 2 : peso + 1;
-    }
-    
-    var digitoVerificador2 = 11 - (soma % 11);
-    cnpj += (digitoVerificador2 < 10) ? digitoVerificador2 : 0;
-    
-    return cnpj;
+  for (var i = 0; i < 12; i++) {
+    cnpj += Math.floor(Math.random() * 10);
   }
-  
-  // Exemplo de uso:
-  var cnpjGerado = gerardorCnpj();
-  console.log(cnpjGerado);
+
+  var soma = 0;
+  var peso = 2;
+    
+  for (var i = 11; i >= 0; i--) {
+    soma += parseInt(cnpj.charAt(i)) * peso;
+    peso = (peso === 9) ? 2 : peso + 1;
+  }
+    
+  var digitoVerificador1 = 11 - (soma % 11);
+  cnpj += (digitoVerificador1 < 10) ? digitoVerificador1 : 0;
+
+  soma = 0;
+  peso = 2;
+    
+  for (var i = 12; i >= 0; i--) {
+    soma += parseInt(cnpj.charAt(i)) * peso;
+    peso = (peso === 9) ? 2 : peso + 1;
+  }
+    
+  var digitoVerificador2 = 11 - (soma % 11);
+  cnpj += (digitoVerificador2 < 10) ? digitoVerificador2 : 0;
+  mostraResultado(cnpj);
+    
+  return cnpj;
+}
+
+function mostraResultado(texto) {
+  document.getElementById("resultado").innerHTML = texto;
+}
